7387e1b7f321f8f50259e3ef63e48b9152a74fa9
Vitaliy <SeekerOfConfessor@gmail.com>
2020-11-05 21:36:33 +0300

    add properties to pom.xml + add Jungle.java and Lion.java

---
+++ src/main/java/ru/itsjava/oop/Lion.java
@@ -0,0 +1,40 @@
+package ru.itsjava.oop;
+
+public class Lion {
+    private String name;
+    private int pawsCount;
+
+    public Lion(String name) {
+        this.name = name;
+        pawsCount = 4;
+    }
+
+    public void printLion() {
+        System.out.println("Лев с именем " + name);
+    }
+
+//    public void setName(String name){
+//        this.name = name;
+//    }
+//
+//    public String getName(){
+//        return name;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPawsCount() {
+        return pawsCount;
+    }
+
+    public void setPawsCount(int pawsCount) {
+        this.pawsCount = pawsCount;
+    }
+
+}
